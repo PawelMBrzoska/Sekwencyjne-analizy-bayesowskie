5738666a92e34a7f1be3fb23b18501505a812c90
Paweł Brzóska <125125417+PawelMBrzoska@users.noreply.github.com>
2023-06-07 23:25:46 +0200

    Plik do pobrania paczek

--- Zainstaluj_BFDA.R
+++ Zainstaluj_BFDA.R
@@ -1,6 +1,8 @@
-#Instalacja wszystkiego
+#KLIKNIJ TUTAJ I NACIŚNIEJ CTRL+ENTER. ALBO "Run" PO PRAWEJ STRONIE MNIEJ-WIĘCEJ.........................................^TUTAJ^
 {
 install.packages("devtools")
+install.packages("haven")
+install.packages("BayesFactor")
 library(devtools)
 install_github("nicebread/BFDA", subdir="package")
 }
